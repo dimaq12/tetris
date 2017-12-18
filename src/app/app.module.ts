b0a1c148e0e13b4909731badf87301f63a39748d
dimaq <dimaq12@ynandex.ru>
2017-12-18 17:11:01 +0200

    'main'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GameInfoComponent } from './components/game-info/game-info.component';
 import { GameComponent } from './components/game/game.component';
+import { TetrisComponent } from './components/tetris/tetris.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GameInfoComponent,
-    GameComponent
+    GameComponent,
+    TetrisComponent
   ],
   imports: [
     BrowserModule
