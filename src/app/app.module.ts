743fc9ee6446a9041d1c3b3711fffd59a6f6febe
dimaq <dimaq12@ynandex.ru>
2017-12-18 15:07:15 +0200

    'init'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GameInfoComponent } from './components/game-info/game-info.component';
+import { GameComponent } from './components/game/game.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameInfoComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
